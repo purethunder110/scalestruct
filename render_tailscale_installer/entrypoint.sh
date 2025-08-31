8f07ab6362e71830dfeb3f6d94c82fcd24d3a9d3
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2025-08-31 21:45:51 +0530

    tailscaue userspace changes

--- render_tailscale_installer/entrypoint.sh
+++ render_tailscale_installer/entrypoint.sh
@@ -2,11 +2,18 @@
 set -e
 
 # Start Tailscale daemon
-/usr/sbin/tailscaled --state=/tmp/tailscale.state --socket=/tmp/tailscaled.sock &
+tailscaled --tun=userspace-networking \
+  --socks5-server=localhost:1055 \
+  --state=/tmp/tailscale.state &
+
 sleep 2
 
 # Bring tailscale up (auth once with a key or interactive)
-tailscale up --authkey=${TAILSCALE_AUTHKEY:-} --hostname=render-proxy
+tailscale up --authkey "$TAILSCALE_AUTH_KEY" --hostname render-web --accept-dns=false
+
+sleep 3
+# checking tailscale status
+tailscale status
 
 # Start HAProxy in foreground
 exec haproxy -f /etc/haproxy/haproxy.cfg -db
