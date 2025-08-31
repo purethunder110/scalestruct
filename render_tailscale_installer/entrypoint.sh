2d342bb411426cc911706533036a15149afc444c
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2025-08-31 20:25:40 +0530

    fixing the systemd errors

--- render_tailscale_installer/entrypoint.sh
+++ render_tailscale_installer/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Start tailscaled in the background
-tailscaled --state=/tmp/tailscale.state &
+# Start tailscaled in userspace networking mode (no TUN/iptables required)
+tailscaled --tun=userspace-networking --state=/tmp/tailscale.state &
 
 # Wait a few seconds for tailscaled to initialize
 sleep 2
 
-# Connect to Tailscale network using auth key
-tailscale up --authkey "$TAILSCALE_AUTH_KEY" --hostname render-web
+# Connect to Tailscale using auth key
+tailscale up --authkey "$TAILSCALE_AUTH_KEY" --hostname render-web --accept-dns=false
 
 # Start nginx in foreground
 nginx -g 'daemon off;'
