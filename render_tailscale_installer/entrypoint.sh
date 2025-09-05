9302c42bc3a9a0138cc941f67d55185774ff1485
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2025-09-06 02:11:51 +0530

    shifting from nginx to caddy

--- render_tailscale_installer/entrypoint.sh
+++ render_tailscale_installer/entrypoint.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-# Start Tailscale daemon
-tailscaled \
-  --tun=userspace-networking 
-  --state=/tmp/tailscale.state &
-
+# Start tailscaled in background
+tailscaled --tun=userspace-networking --state=/tmp/tailscale.state &
 sleep 2
 
-# Bring tailscale up (auth once with a key or interactive)
-tailscale up --authkey "$TAILSCALE_AUTH_KEY" --hostname render-web --accept-dns=false
+# Bring up tailscale
+tailscale up --authkey "${TAILSCALE_AUTH_KEY}" --hostname render-web --accept-dns=false
 
-sleep 3
-# checking tailscale status
-tailscale status
-
-# Start Nginx in foreground
-exec nginx -g 'daemon off;'
+# Start Caddy (foreground)
+exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
