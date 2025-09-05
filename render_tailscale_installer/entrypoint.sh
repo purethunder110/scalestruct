3e1e2239e3bb7acd6681cc0b032116a6d9bd8986
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2025-09-06 01:34:40 +0530

    ampersent hotfix

--- render_tailscale_installer/entrypoint.sh
+++ render_tailscale_installer/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Start Tailscale daemon
 tailscaled \
   --tun=userspace-networking 
-  --state=/tmp/tailscale.state
+  --state=/tmp/tailscale.state &
 
 sleep 2
 
@@ -18,53 +18,3 @@ tailscale status
 
 # Start Nginx in foreground
 exec nginx -g 'daemon off;'
-
-# # # Start HAProxy in foreground
-# # exec haproxy -f /etc/haproxy/haproxy.cfg -db
-# set -eu
-
-# # ---- Defaults from env ----
-# : "${PORT:=8080}"                       # Render usually injects PORT; fallback to 8080 locally
-# : "${TESTBED_PREFIX:=/testbed/}"        # Path prefix to expose
-# : "${TESTBED_DEST:=100.88.146.20:80}"   # Tailscale private target host:port
-# : "${TS_HOSTNAME:=edge-proxy}"          # How this node shows up in Tailscale
-# : "${TAILSCALE_AUTH_KEY:?Set TAILSCALE_AUTH_KEY env var for headless login}"
-
-# echo "[entrypoint] PORT=$PORT"
-# echo "[entrypoint] TESTBED_PREFIX=$TESTBED_PREFIX"
-# echo "[entrypoint] TESTBED_DEST=$TESTBED_DEST"
-# echo "[entrypoint] TS_HOSTNAME=$TS_HOSTNAME"
-
-# # ---- Render Privoxy config ----
-# sed -e "s|127.0.0.1:1055|127.0.0.1:1055|g" \
-#     /etc/privoxy/config.tpl > /etc/privoxy/config
-
-# # ---- Render HAProxy config ----
-# # Escape slashes in prefix for regex usage
-# ESC_PREFIX=$(printf '%s' "$TESTBED_PREFIX" | sed 's/[.[\*^$(){}+?|/]/\\&/g')
-# sed -e "s|\${PORT}|${PORT}|g" \
-#     -e "s|\${TESTBED_PREFIX}|${ESC_PREFIX}|g" \
-#     -e "s|\${TESTBED_DEST}|${TESTBED_DEST}|g" \
-#     /etc/haproxy/haproxy.cfg.tpl > /etc/haproxy/haproxy.cfg
-
-# echo "[entrypoint] Generated /etc/haproxy/haproxy.cfg:"
-# sed 's/^/  /' /etc/haproxy/haproxy.cfg
-
-# echo "[entrypoint] Generated /etc/privoxy/config:"
-# sed 's/^/  /' /etc/privoxy/config
-
-# # ---- Start everything under supervisord ----
-# exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-
-# set -eu
-
-# # Start Tailscale daemon
-# tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-# sleep 2
-# tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=render-web --accept-routes
-
-# # Start Privoxy
-# privoxy --no-daemon /etc/privoxy/config &
-
-# # Start HAProxy in foreground
-# exec haproxy -f /etc/haproxy/haproxy.cfg.tpl
