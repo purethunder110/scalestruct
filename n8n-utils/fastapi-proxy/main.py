c7012d465acbee06243c785aed1b0c0b4f06ffec
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2025-11-16 02:01:15 +0530

    adding requests json for fastapi server

--- n8n-utils/fastapi-proxy/main.py
+++ n8n-utils/fastapi-proxy/main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from jobspy import scrape_jobs
 import numpy as np
+import logging
+
+log = logging.getLogger(__name__)
+
 app = FastAPI()
 
 @app.get("/job_search")
-async def job_search():
+async def job_search(request:Request):
+    # site_name,search_term,google_search_term,location,results_wanted,hours_old,country_indeed
+    json_data = await request.json()
     jobs = scrape_jobs(
-        site_name=["linkedin", "google"], # "glassdoor", "bayt", "naukri", "bdjobs"
-        search_term="Python Django Backend Developer",
-        google_search_term="Django Python Jobs in India since yesterday",
-        location="Noida, Uttar Pradesh",
-        results_wanted=20,
-        hours_old=72,
-        country_indeed='India',
+        **json_data
     )
     jobs = jobs.replace([np.inf, -np.inf, np.nan], None) # replaces Nan to None for json conversion
     return jobs.to_dict(orient="records")
